695defd290c39259b2683dfb0f2a770970e9a203
Arnaldo Liro <dinholiro@gmail.com>
2025-05-16 10:24:16 -0300

    feat: structuring logic to call other functions

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+from sheets_clients import get_sheets_client
+from collector import collect_names
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-print(os.getenv("TESTE"))
+def main():
+    client = get_sheets_client()
+    sheet_name = os.getenv("GOOGLE_SHEET_NAME")
+    worksheet_name = os.getenv("GOOGLE_SHEET_WORKSHEET")
+
+    sheet = client.open(sheet_name).worksheet(worksheet_name)
+    names = collect_names(sheet)
+
+    print("Nomes encontrados:", names)
+
+if __name__ == "__main__":
+    main()
